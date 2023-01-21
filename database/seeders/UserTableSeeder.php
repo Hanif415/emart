12f63e62954be6bccb229df6eb45933a719df17c
hanif415 <hanifamrullah42@gmail.com>
2023-01-21 18:08:42 +0700

    commit

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class UserTableSeeder extends Seeder
 {
@@ -14,12 +16,17 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->DB::insert([
+        DB::table('users')->insert([
+
+            // admin
             [
                 'full_name' => 'Hanif',
                 'username' => 'Admin',
-                'email' => 'admin@gmail.com'
+                'email' => 'admin@gmail.com',
+                'password' => Hash::make('111'),
+                'role' => 'admin',
+                'status' => 'active'
             ]
-        ])
+        ]);
     }
 }
