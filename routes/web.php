ff60f70157474c3e4dc3894b25f090544beac3ac
hanif415 <hanifamrullah42@gmail.com>
2023-01-30 19:09:50 +0700

    fix welcome blade

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,6 +19,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Auth::routes(['register' => false]);
 
 Route::get('/dashboard', function () {
     return view('backend.dashboard');
