adddfc45bb121394aa31c08fd51d6f743eb36e8b
hanif415 <hanifamrullah42@gmail.com>
2023-03-04 07:12:58 +0700

    nothing

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,9 +1,7 @@
 @extends('admin.layouts.main')
 
 @section('content')
-    <?php
-    
-    phpinfo();
-    
-    ?>
+    <div class="d-flex justify content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h5">Dashboard</h1>
+    </div>
 @endsection
